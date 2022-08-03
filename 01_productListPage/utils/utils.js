a66e8b920301eac21f58dfd0f8c4aadca5dfd4dd
Mihai Mascas <mihai.mascas@accesa.eu>
2022-08-03 11:16:16 +0000

    Update utils.js - answers

--- 01_productListPage/utils/utils.js
+++ 01_productListPage/utils/utils.js
@@ -14,6 +14,9 @@ class ProductsController {
     //QUESTION: In the set-up and guidelines i says:When importing the json file, you must treat that as a http request and use the fetch method.
     //I thought that we have to threat this as an actual http get request , what should i do then? 
 
+    // Answer: we are at server level here, so JSON file cand be handled with import rather then rezuesting and endpoint that returns a data from the JSON file.
+    // Consider the JSON file as a rudimentary DB
+
     // move this to axios callback or load the product list sync from directly JSON file
     this._products = allProducts;
     this._products = this.oderByPrice('asc');
@@ -55,10 +58,10 @@ class ProductsController {
       Also,i want to try  make this class functions respect the  single responsability principle 
       
       In my  view this.__products should keep only the products list without any filters
-
-      
       Please,let me what do you think 
       :)
+
+      Answer: this._products will always keep the raw (unsorted/unfiltered) list of products. Your methods (order/filter) will return a NEW list (array) of products. this._products.sort(...) or this._products.filter(...) always creates a new array and does not modifiy the initial array (this._products). So methods are pure.
       */
       return productList.filter(jsonObj => jsonObj.title.toLowerCase().includes(title))
      
