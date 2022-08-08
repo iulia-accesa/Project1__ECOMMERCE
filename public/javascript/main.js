99673abe316186a1823efd85f494ead859e1e3bc
ACCESA\iulia.filimon <Iulia.Filimon@accesa.eu>
2022-08-08 14:22:56 +0300

    solved footer problem

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -3,6 +3,25 @@ let initActions = new ActionsInitializator();
 initActions.initAllActions();
 
 
-
+function   manipulateCartProducts(_action,_productId){
+    //http request
+    const data = { action : _action,
+                  productId : _productId
+                };
+    fetch(MANIPULATE_CART,{
+      method : 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+    })
+    .then((response) => {
+        console.log('Success:', response);
+      })
+      .catch((error) => {
+          console.error('Error:', error);
+        });
+    
+} 
 
   
